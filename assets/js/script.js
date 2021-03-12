00748c58881354d944f64609123589497f13ec62
M. Irfan Junaidi <mirfanjunaidi@gmail.com>
2021-03-12 20:28:33 +0700

    Bug Fix

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+var sidebar = document.getElementById("sidebar");
+var sidebtn = document.getElementById("sidebtn");
+var shade = document.getElementById("shade");
+var x = window.matchMedia("(max-width: 768px)");
+ 
+sidebtn.addEventListener("click", openSidebar);
+shade.addEventListener("click", closeSidebar);
+
+prevent(x);
+x.addListener(prevent);
+
+function openSidebar() {
+    sidebar.classList.add("active");
+    sidebar.style.maxWidth = "300px";
+    shade.style.display = "block";
+}
+    
+function closeSidebar() {
+    sidebar.classList.remove("active");
+    sidebar.style.maxWidth = "0px";
+    shade.style.display = "none";
+}
+
+function prevent(x) {
+    if (x.matches) {
+        sidebar.style.maxWidth = "0px";
+        shade.style.display = "none";
+    } else {
+        sidebar.style.maxWidth = "300px";
+        shade.style.display = "none";
+    }
+}
